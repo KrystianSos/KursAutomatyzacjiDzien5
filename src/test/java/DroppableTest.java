4a16f6d2b10f9fa33e3d08c21a45740e8ecaf824
ala makota <kw.dziubinska@gmail.com>
2019-10-26 12:19:45 +0200

    DroppableTest

--- src/test/java/DroppableTest.java
+++ src/test/java/DroppableTest.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -13,9 +12,29 @@ public class DroppableTest extends TestBase {
        check if "Dropped!" text appeared
 
     */
+    @Test
+    public void droppableTest() {
+        driver.get("http://seleniumui.tc-sii.com/droppable.php");
+        Actions actions = new Actions(driver);
+        actions.clickAndHold(driver.findElement(By.id("draggable")))
+                .moveToElement(driver.findElement(By.id("droppable")))
+                .release()
+                .perform();
 
-    public void droppableTest(){
-        driver.get("http://seleniumui.tc-sii.com/draggable.php");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='droppable']/p")).getText(), "Dropped!");
+
+    }
+
+    @Test
+    public void droppableTest2() {
+        driver.get("http://seleniumui.tc-sii.com/droppable.php");
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(driver.findElement(By.id("draggable")), driver.findElement(By.id("droppable")))
+                .perform();
+
+
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='droppable']/p")).getText(), "Dropped!");
+
     }
 
 }
